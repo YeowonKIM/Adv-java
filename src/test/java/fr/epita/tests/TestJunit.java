4228486cd1792fce3d7e6d4130baf63954cd62c0
YeowonKIM <kkyoo152@gmail.com>
2025-04-28 12:00:44 +0200

    TDD: MathService

--- src/test/java/fr/epita/tests/TestJunit.java
+++ src/test/java/fr/epita/tests/TestJunit.java
@@ -1,11 +1,19 @@
 package fr.epita.tests;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestJunit {
 
     @Test
-    public void test() {
+    public void testFactorial() {
+        // given
+        Integer number = 5;
 
+        // when
+        Integer result = MathService.factorial(number);
+
+        // then
+        Assertions.assertEquals(120, result);
     }
 }
